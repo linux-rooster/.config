606dabb1e9926d73b2da3b58685101cb9ce10448
sir rooster lancelot the third <zaydsaqib20@gmail.com>
2024-12-23 15:06:02 +0400

    Delete fish directory

--- fish/config.fish
+++
@@ -1,7 +0,0 @@
-alias neofetch="neofetch --source ~/.config/neofetch/logo"
-neofetch
-if status is-interactive
-    # Commands to run in interactive sessions can go here
-end
-set fish_greeting
-starship init fish | source
